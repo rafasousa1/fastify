4b33ef1cb6c12ae5a66c4e7ecf6026d02402b5df
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2025-04-25 15:33:20 -0300

    docs: criando regras de negócio

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,12 @@
-import 'dotenv/config'
 import knex from 'knex'
 import { Knex } from 'knex'
+import { env } from './env'
 
-if(!process.env.DATABASE_URL) {
-	throw new Error('DATABASE_URL not found!')
-}
 
 export const config: Knex.Config = { // tipando o config para que siga o formato do Knex.Config
 	client: 'sqlite',
 	connection: {
-		filename: process.env.DATABASE_URL // mostrando url para meu banco de dados
+		filename: env.DATABASE_URL // mostrando url do meu banco de dados
 	},
 	useNullAsDefault: true,
 	migrations: {
