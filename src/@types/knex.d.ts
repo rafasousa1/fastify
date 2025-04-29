941e44dd32c83a5f2bd28d814978da7ceda03e80
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2025-04-29 17:28:21 -0300

    feat: colocando tipagem com o Knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+    export interface Tables { // tipando os campos para melhorar a manipulação no BD 
+        transactions: { // quero que os campos da minha tabela de transactions tenha os seguintes tipos
+        id: string
+        title: string
+        amount: number
+        created_at: number
+        session_id?: number
+        }
+    }
+}
